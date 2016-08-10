7390d60c4b2190b897ce401b5e9ac1a2f3f0a6b0
upa8 <pratikupacharya@gmail.com>
2016-08-10 11:26:35 +0530

    Dropdown added

--- ecommerseWebsite/js/customScripts.js
+++ ecommerseWebsite/js/customScripts.js
@@ -40,3 +40,23 @@ function showSlides() {
     dots[slideIndex-1].className += " active";
     setTimeout(showSlides, 2000); // Change image every 2 seconds
 }
+
+// Open 
+function openTab(evt, idName) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(idName).style.display = "block";
+    evt.currentTarget.className += " active";
+}
+
+function closeTab(idName) {
+    document.getElementById(idName).style.display = "none";
+    //evt.currentTarget.className += " active";
+}
