f303756a6d39ae02164fd66476606a53eefc4487
upa8 <pratikupacharya@gmail.com>
2016-09-06 09:26:54 +0530

    login validation implemented

--- EcomProject/WebContent/AngularJs/js/controllers/loginController.js
+++ EcomProject/WebContent/AngularJs/js/controllers/loginController.js
@@ -8,7 +8,6 @@ function loginController($scope,restApi,$location, $window,$rootScope,appFactory
 		var password = $scope.password;
 		// calling rest api
 		restApi.login(userName,password).then(function(result){
-			
 			//console.log("User is loggedIn");
 			console.log("Response is " + JSON.stringify(result, null,4));
 			//console.log(result.data.userId);
@@ -16,17 +15,23 @@ function loginController($scope,restApi,$location, $window,$rootScope,appFactory
 			// Store userInformation in localStorage of angularJS $window service 
 			var authToken = result.data['auth-token'];
 			var userId = result.data.userId;
-			//console.log("Auth token " + authToken +" UserId "+ userId);
-			$window.localStorage.setItem('authToken', authToken);
-			$window.localStorage.setItem('userId', userId);
-			// set the header as loggedIn
-			
-			$rootScope.userLoggedIn = true;
-			$rootScope.userId = userId;
-			$rootScope.authToken = authToken;
-			
-			$location.path("/");
+			if(authToken != null){
+
+				//console.log("Auth token " + authToken +" UserId "+ userId);
+				$window.localStorage.setItem('authToken', authToken);
+				$window.localStorage.setItem('userId', userId);
+				// set the header as loggedIn
+				
+				$rootScope.userLoggedIn = true;
+				$rootScope.userId = userId;
+				$rootScope.authToken = authToken;
+				
+				$location.path("/");
+			}else{
+				$scope.errorMessage = "Wrong username or password !!!";
+			}
 		},function(err){
+			$scope.errorMessage = JSON.stringify(err, null, 4);
 			//console.log("There is problem in login");
 			//$scope.userLoggedIn = "Request unsuccefull and user is not log";
 		});	
@@ -36,7 +41,7 @@ function loginController($scope,restApi,$location, $window,$rootScope,appFactory
 	// userSignUp
 	
 	$scope.register = function(){
-		//console.log("Clicked register");
+		console.log("Clicked register");
 		var regUser = {
 				regFirstName : $scope.regFirstName,
 				regLastName : $scope.regLastName,
@@ -46,10 +51,14 @@ function loginController($scope,restApi,$location, $window,$rootScope,appFactory
 				regPass : $scope.regPass,
 				regConfirmPass : $scope.regConfirmPass
 		}
+		console.log("Input given "+ JSON.stringify(regUser,null,4));
 		// calling rest api
 		restApi.register(regUser).then(function(result) {			
 			//$scope.userLoggedIn = "Request successful and user is registered";		
+			$scope.errorMessage = "User is registered !!!";
 		},function(err) {
+			$scope.errorMessage = err.responseJSON.data.error;//JSON.stringify(err);
+			
 			//console.log("There is problem in login");
 			//$scope.userLoggedIn = "Request unsuccefull and user is not registered";
 		});		
@@ -58,6 +67,7 @@ function loginController($scope,restApi,$location, $window,$rootScope,appFactory
 	
 	// logout 
 	$scope.logout = function(){
+		
 		//console.log("logout button clicked");
 		var authToken = $window.localStorage.getItem('authToken');
 		restApi.logout(authToken).then(function(result){
