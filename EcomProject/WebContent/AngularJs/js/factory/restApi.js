c81b6dd0c700305a1eb48f940f347ecf4c7363b2
upa8 <pratikupacharya@gmail.com>
2016-09-07 04:49:11 +0530

    Form updated

--- EcomProject/WebContent/AngularJs/js/factory/restApi.js
+++ EcomProject/WebContent/AngularJs/js/factory/restApi.js
@@ -1,6 +1,6 @@
 function restApi( $q , $rootScope) {
 	var baseUrl =  "http://124.124.83.165:9000";//'http://10.20.14.83:9000';
-	var limitRecords = '?records=5';
+	var limitRecords = 'records=2';
 	console.log("Rest api Initiated");
 	var restApi = {};
 
